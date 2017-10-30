3c80b334912f29734c8c7a1fbe14d0a4375f835e
liuna0113 <390367635@qq.com>
2017-10-30 10:36:51 +0800

    新建分类,产品,用户页面,并且index完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,4 +1,19 @@
 $(function () {
+  //在每次要访问数据之前都传送一下$.ajax请求,但是由于login.html页面是需要直接进的,不需要验证
+  if (location.href.indexOf('login.html') < 0) {
+    $.ajax({
+      type: "get",
+      url: "/employee/checkRootLogin",
+      success: function (data) {
+        if (data.error === 400) {
+          // alert('您尚未登录,请登录');
+          window.location.href = "login.html";
+        }
+      }
+    })
+  }
+
+
   $(document).ajaxStart(function () {
     NProgress.start();
   });
@@ -7,40 +22,44 @@ $(function () {
     NProgress.done();
   }, 500);
   
+  //分类管理被点击的时候 显示孩子们
+  // $('.cata').on('click', function () {
+  //   $('.child').slideToggle();
+  // });
   
-  $('.cata').on('click', function () {
-    $('.child').slideToggle();
-  });
+  $('.child').prev().on('click', function () {
+    $(this).next().slideToggle();
+  })
 
+  //点击子分类的时候,上边的也不隐藏
+  // $('.child').child.on('click', function () {
+  //   // $(this).slideToggle();
+  // });
+
+  //左边栏目向左边跑
   $('.lt_menu').on('click', function () {
     $('.lt_aside').toggleClass('now');
     $('.lt_main').toggleClass('now');
   });
 
-  // $('.lt_logout').on('click', function () {
-  //   $.ajax({
-  //     type: "get",
-  //     url: "/employee/checkRootLogin",
-  //     success: function (data) {
-  //       if (data.success) {
-  //         window.location.href = "login.html";
-  //       }
-  //     }
-  //   })
-  // });
+  //下面的退出功能是公用的
+  $('.lt_logout').on('click', function () {
+    //使用$(myModal)的modal(show)方法,使modal显示出来,而不是直接用show
+    $('#myModal').modal("show");
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  $('.confirm_logout').on('click', function () {
+    $.ajax({
+      type: "get",
+      url: "/employee/employeeLogout",
+      success: function (data) {
+        console.log(data);
+        if (data.success) {
+          window.location.href = "login.html";
+        }
+      }
+    })
+  })
+  
 
 })
