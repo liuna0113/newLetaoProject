245ddc92ce7b435de1435b2e119b069f2acc5f28
liuna0113 <390367635@qq.com>
2017-10-30 09:26:58 +0800

    增加了index修改项

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,6 +1,46 @@
-$(document).ajaxStart(function () {
-  NProgress.start();
-});
-setTimeout(function () {
-  NProgress.stop();
-}, 500)
+$(function () {
+  $(document).ajaxStart(function () {
+    NProgress.start();
+  });
+  
+  setTimeout(function () {
+    NProgress.done();
+  }, 500);
+  
+  
+  $('.cata').on('click', function () {
+    $('.child').slideToggle();
+  });
+
+  $('.lt_menu').on('click', function () {
+    $('.lt_aside').toggleClass('now');
+    $('.lt_main').toggleClass('now');
+  });
+
+  // $('.lt_logout').on('click', function () {
+  //   $.ajax({
+  //     type: "get",
+  //     url: "/employee/checkRootLogin",
+  //     success: function (data) {
+  //       if (data.success) {
+  //         window.location.href = "login.html";
+  //       }
+  //     }
+  //   })
+  // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
